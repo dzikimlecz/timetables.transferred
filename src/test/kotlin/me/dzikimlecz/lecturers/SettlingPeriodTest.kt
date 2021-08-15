bd20f9ba44bef805b8510dc8c8568289615153cc
dzikimlecz <j.pul312@gmail.com>
2021-08-15 21:18:28 +0200

    removed double package declaration

--- src/test/kotlin/me/dzikimlecz/lecturers/SettlingPeriodTest.kt
+++ src/test/kotlin/me/dzikimlecz/lecturers/SettlingPeriodTest.kt
@@ -1,7 +1,5 @@
 package me.dzikimlecz.lecturers
 
-package me.dzikimlecz.lecturers
-
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
